b5f52a18a4bdd0d213827bf3ee61333d33a53c6c
Kirito-YO <ussf.lord@gmail>
2023-12-12 06:14:16 -0800

    update

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,32 +1,18 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
 
-modulel.exports = class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
-    }
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  }
 
-    print () {
-        let str = '';
-        let w = this.width;
-        let h = this.height;
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
 
-        while (w > 0) {
-            str += 'X';
-            w--;
-        }
-        while (h > 0) {
-            console.log(str);
-            h--;
-        }
-    }
-    rotate () {
-        [this.width, this.height] = [this.height, this.width];
-    }
-    double () {
-        this.width *= 2;
-        this.height *= 2;
-    }
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
 };
