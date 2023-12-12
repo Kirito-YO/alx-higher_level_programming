2dcc8d1222633a1c24b4c8e02fc2a270fa31be07
Kirito-YO <ussf.lord@gmail>
2023-12-12 06:17:20 -0800

    update

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
-
-exports.init = function (list) {
-    const newList = [];
-    let index = 0;
-    const len = list.length;;
-    while (index < len) {
-        newList.push(list.pop());
-        index++;
-    }
-    return newList;
+exports.esrever = function (list) {
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
 };
