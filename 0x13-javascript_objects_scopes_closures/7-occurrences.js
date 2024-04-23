ace468db1a056e193fe51ae3e649b264ae0d0384
Kirito-YO <ussf.lord@gmail>
2024-04-23 11:46:59 -0700

    js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-exports.nbOccurrences = function ( list, searchElement ) {
-    const newList = list.filter( element => element === searchElement );
-    return newList.length;
-}
