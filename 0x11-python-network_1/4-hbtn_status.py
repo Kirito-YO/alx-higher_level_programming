a456cfe4f875a6958e1dfcd267a12b76f4f34a45
Kirito-YO <ussf.lord@gmail.com>
2024-05-10 15:26:47 -0700

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+"""
+This script fetches "https://alx-intranet.hbtn.io/status"
+"""
 import requests
 
-
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+r = requests.get('https://alx-intranet.hbtn.io/status')
+value = r.content.decode('utf-8')
+print('Body response:')
+print(f"\t- type: {type(value)}")
+print(f"\t- content: {value}")
